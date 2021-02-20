823381a41f5b9a3389c6c538ec9e84824d477aa4
Xzender Cage <37167113+xzendercage@users.noreply.github.com>
2021-02-20 16:30:15 +0530

    Update _plugin.js

--- plugins/_plugin.js
+++ plugins/_plugin.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2021 XZENDER CAGE 
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+NISA - XZENDER CAGE 
 */
 
 const Asena = require('../events');
